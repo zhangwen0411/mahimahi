04f105a3d53a0b8ca149aa73698585aba96c0485
Wen Zhang <zhangwen@cs.stanford.edu>
2017-05-25 14:06:51 -0700

    http proxy sends sni servername

--- src/httpserver/secure_socket.hh
+++ src/httpserver/secure_socket.hh
@@ -29,6 +29,10 @@ public:
 
     std::string read( void );
     void write( const std::string & message );
+
+    /* if an SNI servername has been received, assigns it to servername and returns true */
+    bool get_sni_servername( std::string & servername );
+    void set_sni_servername_sent( const std::string & servername );
 };
 
 class SSLContext
@@ -37,6 +41,9 @@ private:
     struct CTX_deleter { void operator()( SSL_CTX * x ) const { SSL_CTX_free( x ); } };
     typedef std::unique_ptr<SSL_CTX, CTX_deleter> CTX_handle;
     CTX_handle ctx_;
+
+    static void set_servername( SSL * ssl, const std::string & servername );
+    static bool get_servername( SSL * ssl, std::string & servername );
 
 public:
     SSLContext( const SSL_MODE type );
